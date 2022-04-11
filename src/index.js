bf0ffdd503750a66e67c333d33a1d31c38b64663
Sol Pichirili <sol.pichirili@gmail.com>
2022-04-11 20:04:56 -0300

    cluster

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 const options = require('./config');
 const logger = require('./utils/winston');
 const httpServer = require('./server');
+const cluster = require('cluster');
+const numCPUS = require('os').cpus().length;
 
-let port = options.port;
+const isCluster = options.mode === 'CLUSTER';
 
-const app = httpServer.listen(port, () => {
-    logger.info(`Servidor corriendo en ${port}`);
-});
+if (cluster.isMaster && isCluster) {
+    logger.info(`Cantidad de procesadores ${numCPUS}`);
+    logger.info(`PID MASTER ${process.pid}`);
 
-app.on('error', (error) => {
-    logger.error(`Error: ${error}`);
-});
+    for (let i = 0; i < numCPUS; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', worker => {
+        logger.info(`Worker ${worker.process.pid} died ${new Date().toLocaleString()}`);
+        cluster.fork();
+    })
+} else {
+    let port = options.port || process.argv[2];
+
+    const app = httpServer.listen(port, () => {
+        logger.info(`Cantidad de procesadores ${numCPUS}`);
+        logger.info(`PID MASTER ${process.pid}`);
+        logger.info(`Funcionando en puerto ${port}`);
+    });
+
+    app.on('error', (error) => {
+        logger.error(`Error: ${error}`);
+    });
+}
